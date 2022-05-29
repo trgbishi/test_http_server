208435aa6673d3c1daa74b396b8614ff23aec949
trgbishi <trgbishi@yeah.net>
2022-05-29 18:46:13 +0800

    测试服务器更新

--- server1.js
+++ server1.js
@@ -4,17 +4,34 @@ const app = express()
 app.use((request,response,next)=>{
 	console.log('有人请求服务器1了');
 	// console.log('请求来自于',request.get('Host'));
-	// console.log('请求的地址',request.url);
+	console.log('请求的地址',request.url);
 	next()
 })
 
-app.get('/students',(request,response)=>{
-	const students = '{"data": [{"time": "2018/09/20","data": "1"}, {"time": "2018/09/21","data": "1"},{"time": "2018/09/22","data": "1"},{"time": "2018/09/23","data": "1"},{"time": "2018/09/24","data": "1"},{"time": "2018/09/25","data": "1"},{"time": "2018/09/26","data": "1"},{"time": "2018/09/27","data": "2"},{"time": "2018/09/28","data": "1"},{"time": "2018/09/29","data": "3"},{"time": "2018/09/30","data": "3"}]}'
+app.get('/api/getLeaseUnitPrice',(request,response)=>{
+	const students = '[{"time": "2018/09/20 00:00:00","data": "1"}, {"time": "2018/09/20 05:00:00","data": "1"},{"time": "2018/09/20 08:00:00","data": "1"},{"time": "2018/09/20 14:00:00","data": "1"},{"time": "2018/09/20 18:00:00","data": "1"},{"time": "2018/09/20 23:00:00","data": "1"},{"time": "2018/09/21 06:00:00","data": "1"},{"time": "2018/09/21  12:00:00","data": "2"},{"time": "2018/09/21 16:00:00","data": "1"},{"time": "2018/09/21 20:00:00","data": "3"},{"time": "2018/09/22 00:00:00","data": "3"}]'
+	response.send(students)
+})
+app.get('/api/getOnSaleCount',(request,response)=>{
+	const students = '[{"time": "2018/09/20 00:00:00","data": "1"}, {"time": "2018/09/20 01:00:00","data": "1"},{"time": "2018/09/20 02:00:00","data": "1"},{"time": "2018/09/20 03:00:00","data": "1"},{"time": "2018/09/20 04:00:00","data": "1"},{"time": "2018/09/20 05:00:00","data": "1"},{"time": "2018/09/20 06:00:00","data": "1"},{"time": "2018/09/20  07:00:00","data": "2"},{"time": "2018/09/20 08:00:00","data": "1"},{"time": "2018/09/20 09:00:00","data": "3"},{"time": "2018/09/20 10:00:00","data": "3"}]'
+	response.send(students)
+})
+app.get('/api/getPrice',(request,response)=>{
+	const students = '[{"time": "2018/09/20 00:00:00","data": "1"}, {"time": "2018/09/21 00:00:00","data": "1"},{"time": "2018/09/22 00:00:00","data": "1"},{"time": "2018/09/23 00:00:00","data": "1"},{"time": "2018/09/24 00:00:00","data": "1"},{"time": "2018/09/25 00:00:00","data": "1"},{"time": "2018/09/26 00:00:00","data": "1"},{"time": "2018/09/27","data": "2"},{"time": "2018/09/28 00:00:00","data": "1"},{"time": "2018/09/29","data": "3"},{"time": "2018/09/30","data": "3"}]'
+	response.send(students)
+})
+app.get('/api/getRatio',(request,response)=>{
+	const students = '[{"time": "2018/09/20 00:00:00","data": "1"}, {"time": "2018/09/21 00:00:00","data": "1"},{"time": "2018/09/22 00:00:00","data": "1"},{"time": "2018/09/23 00:00:00","data": "1"},{"time": "2018/09/24 00:00:00","data": "1"},{"time": "2018/09/25 00:00:00","data": "1"},{"time": "2018/09/26 00:00:00","data": "1"},{"time": "2018/09/27","data": "2"},{"time": "2018/09/28 00:00:00","data": "1"},{"time": "2018/09/29","data": "3"},{"time": "2018/09/30","data": "3"}]'
 	response.send(students)
 })
 
-app.get('/items',(request,response)=>{
-	const items = '{"items":[{"name":"饰品11111111111111111111111111111111","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100},{"name":"饰品1","price":"100","rent":"1","ratio":"111","count":100}]}'
+app.get('/api/trade/Order/GetTopLeaseOutOrderList',(request,response)=>{
+	const students = '{"Code":0,"Msg":"请求成功","TipType":10,"Data":[{"IconUrl":"https://youpin.img898.com/economy/image/4e036200613911ec8289acde48001122","LeaseUnitPrice":0.01,"LeaseDays":8,"LeaseDeposit":0.10,"Type":2,"DateTime":"2022.05.21","UpdateTime":null}]}'
+	response.send(students)
+})
+
+app.post('/api/getRank',(request,response)=>{
+	const items = '[{"id":"1","commodityName":"饰品11111111111111111111111111111111","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100},{"id":"1","commodityName":"饰品1","price":"100","leaseUnitPrice":"1","ratio":"111","onSaleCount":100}]'
 	response.send(items)
 })
 
